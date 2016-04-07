70ee5393bce839c7039e681bbece18eef9f3f5c1
Danilo Caballero <fabrizio.dfc2@gmail.com>
2016-04-07 16:37:40 -0700

    Add Angular 2 (#14)
    * Adding packages
    * Refactoring porject structure in preparation to Angular2
    * Basic app structure
    * Angularization completed

---
+++ public/app/app.component.js
@@ -0,0 +1,10 @@
+import './app.component.scss';
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'ng2-playground',
+  template: '<h1>Hello, World!</h1>',
+})
+export class AppComponent {
+
+}
