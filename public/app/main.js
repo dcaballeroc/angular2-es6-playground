e2d0cb646ea2bd78418979fad0861fed701e5e7d
Danilo Caballero <fabrizio.dfc2@gmail.com>
2016-05-06 11:54:39 -0700

    Removing unnecessary imports

--- public/app/main.js
+++ public/app/main.js
@@ -1,13 +1,9 @@
 import './main.scss';
 
 import 'core-js/es6';
-import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
+import 'core-js/es7/reflect';
 
-import 'angular2/platform/browser';
-import 'angular2/platform/common_dom';
-import 'angular2/core';
-import 'angular2/router';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
